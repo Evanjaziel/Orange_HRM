53a31034e145d4d07ef2af8d9daee92bacea9bce
Evan <evanjaziel@hotmail.com>
2024-08-28 18:03:54 -0600

    Configuración inicial de CI con GitHub Actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "x9pp5m",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
